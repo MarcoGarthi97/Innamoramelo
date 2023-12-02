4384f809dfb04c985820aa49d5ed00230e96d583
MarcoGarthi97 <marcogarthi@gmail.com>
2023-12-02 18:43:14 +0100

    Add LikeAPI and Controller

---
+++ InnamorameloAPI/Controllers/LikeController.cs
@@ -0,0 +1,227 @@
+﻿using InnamorameloAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnamorameloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LikeController : ControllerBase
+    {
+        static private AuthenticationAPI auth = new AuthenticationAPI();
+        static private MyBadRequest badRequest = new MyBadRequest();
+
+        [HttpGet("GetLike", Name = "GetLike")]
+        public ActionResult<LikeDTO> GetLike(string id)
+        {
+            try
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+                {
+                    var userDTO = auth.GetUserByToken(authHeader);
+                    if (userDTO != null)
+                    {
+                        var likeAPI = new LikeAPI();
+                        var likes = likeAPI.GetAllLike(userDTO.Id);
+                        var like = likes.FirstOrDefault(x => x.Id == id);
+
+                        if(like != null)
+                            return Ok(like);
+                    }
+                    else
+                        return badRequest.CreateBadRequest("Unauthorized", "User not authorizated", 404);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+
+        [HttpGet("GetLikebyId", Name = "GetLikebyId")]
+        public ActionResult<LikeDTO> GetLikebyId(string id)
+        {
+            try
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+                {
+                    if (auth.CheckLevelUserByToken(authHeader))
+                    {
+                        var likeAPI = new LikeAPI();
+                        var like = likeAPI.GetLike(id);
+
+                        if (like != null)
+                            return Ok(like);
+                    }
+                    else
+                        return badRequest.CreateBadRequest("Unauthorized", "User not authorizated", 404);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+
+        [HttpGet("GetAllLikes", Name = "GetAllLikes")]
+        public ActionResult<List<LikeDTO>> GetAllLikes()
+        {
+            try
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+                {
+                    var userDTO = auth.GetUserByToken(authHeader);
+                    if (userDTO != null)
+                    {
+                        var likeAPI = new LikeAPI();
+                        var likes = likeAPI.GetAllLike(userDTO.Id);
+
+                        if (likes != null)
+                            return Ok(likes);
+                    }
+                    else
+                        return badRequest.CreateBadRequest("Unauthorized", "User not authorizated", 404);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+
+        [HttpPost("InsertLike", Name = "InsertLike")]
+        public ActionResult<LikeDTO> InsertLike(LikeInsertModel likeInsert)
+        {
+            try
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+                {
+                    if (auth.CheckLevelUserByToken(authHeader))
+                    {
+                        var likeDTO = new LikeDTO();
+                        Validator.CopyProperties(likeInsert, likeDTO);
+
+                        var likeAPI = new LikeAPI();
+                        var likes = likeAPI.GetAllLike(likeDTO.UserId);
+                        var like = likes.FirstOrDefault(x => x.UserIdLiked == likeDTO.UserIdLiked);
+
+                        if(like == null)
+                        {
+                            var insert = likeAPI.InsertLike(likeDTO);
+
+                            if (insert)
+                            {
+                                likes = likeAPI.GetAllLike(likeDTO.UserId);
+                                like = likes.FirstOrDefault(x => x.UserIdLiked == likeDTO.UserIdLiked);
+
+                                if (like != null)
+                                    return Ok(like);
+                            }
+                        }                        
+                    }
+                    else
+                        return badRequest.CreateBadRequest("Unauthorized", "User not authorizated", 404);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+
+        [HttpPatch("UpdateLike", Name = "UpdateLike")]
+        public ActionResult<LikeDTO> UpdateLike(LikeUpdateModel likeUpdate)
+        {
+            try
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+                {
+                    if (auth.CheckLevelUserByToken(authHeader))
+                    {
+                        var likeAPI = new LikeAPI();
+                        var update = likeAPI.UpdateLike(likeUpdate);
+
+                        if (update != null)
+                            return Ok(update);
+                    }
+                    else
+                        return badRequest.CreateBadRequest("Unauthorized", "User not authorizated", 404);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+
+        [HttpDelete("DeleteLike", Name = "DeleteLike")]
+        public ActionResult<bool> DeleteLike(LikeDeleteModel likeDelete)
+        {
+            try
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+                {
+                    if (auth.CheckLevelUserByToken(authHeader))
+                    {
+                        var likeAPI = new LikeAPI();
+                        var likes = likeAPI.GetAllLike(likeDelete.UserId);
+                        var like = likes.FirstOrDefault(x => x.Id == likeDelete.Id);
+
+                        if (like != null)
+                        {
+                            var delete = likeAPI.DeleteLike(likeDelete.Id);
+
+                            if (delete != null)
+                                return Ok(delete);
+                        }
+                    }
+                    else
+                        return badRequest.CreateBadRequest("Unauthorized", "User not authorizated", 404);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+
+        [HttpDelete("DeleteAllLikes", Name = "DeleteAllLikes")]
+        public ActionResult<bool> DeleteAllLikes()
+        {
+            try
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+                {
+                    var userDTO = auth.GetUserByToken(authHeader);
+                    if (userDTO != null)
+                    {
+                        var likeAPI = new LikeAPI();
+                        var delete = likeAPI.DeleteLikesByUserId(userDTO.Id);
+
+                        if (delete != null)
+                            return Ok(delete);
+                    }
+                    else
+                        return badRequest.CreateBadRequest("Unauthorized", "User not authorizated", 404);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+    }
+}
---
+++ InnamorameloAPI/Models/LikeAPI.cs
@@ -0,0 +1,228 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using MongoDB.Driver;
+
+namespace InnamorameloAPI.Models
+{
+    public class LikeAPI
+    {
+        static private MongoAPI mongo = new MongoAPI();
+
+        internal LikeDTO? GetLike(string id)
+        {
+            try
+            {
+                IMongoDatabase innamoramelo = mongo.GetDatabase();
+                IMongoCollection<LikeMongoDB> likes = innamoramelo.GetCollection<LikeMongoDB>("Likes");
+
+                var filter = Builders<LikeMongoDB>.Filter.Eq(x => x.Id, new ObjectId(id));
+                var find = likes.Find(filter).FirstOrDefault();
+
+                var like = new LikeDTO();
+                Validator.CopyProperties(find, like);
+
+                return like;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+        internal List<LikeDTO>? GetAllLike(string userId)
+        {
+            try
+            {
+                IMongoDatabase innamoramelo = mongo.GetDatabase();
+                IMongoCollection<LikeMongoDB> likes = innamoramelo.GetCollection<LikeMongoDB>("Likes");
+
+                var filter = Builders<LikeMongoDB>.Filter.Eq(x => x.UserId, new ObjectId(userId));
+                var find = likes.Aggregate().Match(filter).ToList();
+
+                var likeList = new List<LikeDTO>();
+
+                foreach (var likeFind in find)
+                {
+                    var like = new LikeDTO();
+                    Validator.CopyProperties(likeFind, like);
+
+                    likeList.Add(like);
+                }
+
+                return likeList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return new List<LikeDTO>();
+        }
+
+        internal bool IsMatched(string userId, string userIdLiked)
+        {
+            try
+            {
+                IMongoDatabase innamoramelo = mongo.GetDatabase();
+                IMongoCollection<LikeMongoDB> likes = innamoramelo.GetCollection<LikeMongoDB>("Likes");
+
+                var filter = Builders<LikeMongoDB>.Filter.Eq(x => x.UserId, new ObjectId(userId));
+                filter &= Builders<LikeMongoDB>.Filter.Eq(x => x.UserIdLiked, new ObjectId(userIdLiked));
+                var find = likes.Find(filter).FirstOrDefault();
+
+                if (find.IsLiked.Value)
+                {
+                    filter = Builders<LikeMongoDB>.Filter.Eq(x => x.UserId, new ObjectId(userIdLiked));
+                    filter &= Builders<LikeMongoDB>.Filter.Eq(x => x.UserIdLiked, new ObjectId(userId));
+                    find = likes.Find(filter).FirstOrDefault();
+
+                    if (find.IsLiked.Value)
+                        return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        internal bool InsertLike(LikeDTO likeDTO)
+        {
+            try
+            {
+                var like = new LikeMongoDB();
+                Validator.CopyProperties(likeDTO, like);
+
+                IMongoDatabase innamoramelo = mongo.GetDatabase();
+                IMongoCollection<LikeMongoDB> likes = innamoramelo.GetCollection<LikeMongoDB>("Likes");
+
+                likes.InsertOne(like);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        internal LikeDTO? UpdateLike(LikeUpdateModel like)
+        {
+            try
+            {
+                IMongoDatabase innamoramelo = mongo.GetDatabase();
+                IMongoCollection<LikeMongoDB> likes = innamoramelo.GetCollection<LikeMongoDB>("Likes");
+
+                var filter = Builders<LikeMongoDB>.Filter.Eq(x => x.Id, new ObjectId(like.Id));
+
+                var updateDefinition = new List<UpdateDefinition<LikeMongoDB>>();
+
+                if (like.IsLiked != null)
+                    updateDefinition.Add(Builders<LikeMongoDB>.Update.Set("IsLiked", like.IsLiked));
+
+                var updateLike = Builders<LikeMongoDB>.Update.Combine(updateDefinition);
+                var update = likes.UpdateOne(filter, updateLike);
+
+                var likeUpdated = GetLike(like.Id);
+                return likeUpdated;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return null;
+            }
+        }
+
+        internal bool DeleteLike(string id)
+        {
+            try
+            {
+                IMongoDatabase innamoramelo = mongo.GetDatabase();
+                IMongoCollection<LikeMongoDB> likes = innamoramelo.GetCollection<LikeMongoDB>("Likes");
+
+                var filter = Builders<LikeMongoDB>.Filter.Eq(x => x.Id, new ObjectId(id));
+                likes.DeleteOne(filter);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        internal bool DeleteLikesByUserId(string userId)
+        {
+            try
+            {
+                IMongoDatabase innamoramelo = mongo.GetDatabase();
+                IMongoCollection<LikeMongoDB> likes = innamoramelo.GetCollection<LikeMongoDB>("Likes");
+
+                var filter = Builders<LikeMongoDB>.Filter.Eq(x => x.UserId, new ObjectId(userId));
+                likes.DeleteMany(filter);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+    }
+
+    public class LikeMongoDB : Like
+    {
+        [BsonIgnoreIfDefault]
+        [JsonConverter(typeof(ObjectIdConverter))]
+        public ObjectId Id { get; set; }
+        [BsonIgnoreIfDefault]
+        [JsonConverter(typeof(ObjectIdConverter))]
+        public ObjectId UserId { get; set; }
+        [BsonIgnoreIfDefault]
+        [JsonConverter(typeof(ObjectIdConverter))]
+        public ObjectId UserIdLiked { get; set; }
+    }
+
+    public class LikeDTO : Like
+    {
+        public string? Id { get; set; }
+        public string? UserId { get; set; }
+        public string? UserIdLiked { get; set; }
+    }
+
+    public class LikeInsertModel : Like
+    {
+        public string? UserId { get; set; }
+        public string? UserIdLiked { get; set; }
+    }
+
+    public class LikeUpdateModel
+    {
+        public string? Id { get; set; }
+        public bool? IsLiked { get; set; }
+    }
+
+    public class LikeDeleteModel
+    {
+        public string? Id { get; set; }
+        public string? UserId { get; set; }
+    }
+
+    public class Like
+    {
+        public DateTime? Created { get; set; }
+        public bool? IsLiked { get; set; }
+    }
+}
