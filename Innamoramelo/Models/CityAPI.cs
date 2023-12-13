18686eea76961d6d18383f2e96666474ec0dc604
MarcoGarthi97 <marcogarthi@gmail.com>
2023-12-13 23:35:08 +0100

    Add API for city

---
+++ Innamoramelo/Models/CityAPI.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace Innamoramelo.Models
+{
+    public class CityAPI
+    {
+        private string UrlAPI { get; set; }
+        public CityAPI(IConfiguration config)
+        {
+            UrlAPI = config["urlAPI"] + "/City/";
+        }
+
+        internal async Task<List<CityDTO>?> GetCity(string filter, string token)
+        {
+            try
+            {
+                var options = new RestClientOptions(UrlAPI)
+                {
+                    MaxTimeout = -1,
+                };
+
+                var client = new RestClient(options);
+
+                var request = new RestRequest("GetCity?filter=" + filter, Method.Get);
+                request.AddHeader("Authorization", "Bearer " + token);
+
+                RestResponse response = await client.ExecuteAsync(request);
+
+                var citiesDTO = JsonConvert.DeserializeObject<List<CityDTO>?>(response.Content);
+                return citiesDTO;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
