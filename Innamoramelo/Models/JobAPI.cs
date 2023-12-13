6278a3e05de8d476a6fe70daabbfae116a055251
MarcoGarthi97 <marcogarthi@gmail.com>
2023-12-13 23:35:25 +0100

    Add API for city

---
+++ Innamoramelo/Models/JobAPI.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace Innamoramelo.Models
+{
+    public class JobAPI
+    {
+        private string UrlAPI { get; set; }
+        public JobAPI(IConfiguration config)
+        {
+            UrlAPI = config["urlAPI"] + "/Job/";
+        }
+
+        internal async Task<List<JobDTO>?> GetJob(string filter, string token)
+        {
+            try
+            {
+                var options = new RestClientOptions(UrlAPI)
+                {
+                    MaxTimeout = -1,
+                };
+
+                var client = new RestClient(options);
+
+                var request = new RestRequest("GetJob?filter=" + filter, Method.Get);
+                request.AddHeader("Authorization", "Bearer " + token);
+
+                RestResponse response = await client.ExecuteAsync(request);
+
+                var jobsDTO = JsonConvert.DeserializeObject<List<JobDTO>?>(response.Content);
+                return jobsDTO;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Innamoramelo/Models/JobDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Innamoramelo.Models
+{
+    public class JobDTO
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
