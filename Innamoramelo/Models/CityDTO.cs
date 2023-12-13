4fb131bd652bd4d07aca45151316f6b55e34266d
MarcoGarthi97 <marcogarthi@gmail.com>
2023-12-13 23:35:20 +0100

    Add API for city

---
+++ Innamoramelo/Models/CityDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Innamoramelo.Models
+{
+    public class CityDTO
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Province { get; set; }
+        public string? Region { get; set; }
+        public string? ProvinceTitle { get; set; }
+    }
+}
