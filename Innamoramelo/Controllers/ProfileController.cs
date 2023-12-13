4288536c17d935e7a451504d55e7fdb6d518e23c
MarcoGarthi97 <marcogarthi@gmail.com>
2023-12-13 23:34:49 +0100

    Update profile controller

--- Innamoramelo/Controllers/ProfileController.cs
+++ Innamoramelo/Controllers/ProfileController.cs
@@ -1,21 +1,59 @@
 ﻿using Innamoramelo.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Innamoramelo.Controllers
 {
-    public class ProfileController : Controller
+    public class ProfileController : AuthenticationController
     {
-        public JsonResult GetJobs(string filter)
+        public ProfileController(IConfiguration _config) : base(_config)
         {
-            
-
-            return Json("");
+            Config = _config;
         }
 
-        public JsonResult GetMunicipality(string filter)
+        public ActionResult<List<JobDTO>?> GetJobs(string filter)
         {
-            
-            return Json("");
+            try
+            {
+                if(filter.Length > 2)
+                {
+                    AuthenticationAdmin();
+
+                    var jobAPI = new JobAPI(Config);
+                    var jobs = jobAPI.GetJob(filter, TokenAdmin).Result;
+
+                    return Ok(jobs);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
+        }
+
+        public ActionResult<List<CityDTO>?> GetCity(string filter)
+        {
+            try
+            {
+                if (filter.Length > 2)
+                {
+                    AuthenticationAdmin();
+
+                    var cityAPI = new CityAPI(Config);
+                    var cities = cityAPI.GetCity(filter, TokenAdmin).Result;
+
+                    return Ok(cities);
+                }
+            }
+            catch (Exception ex)
+            {
+                return badRequest.CreateBadRequest("Internal Server Error", "An internal error occurred.", 500);
+            }
+
+            return badRequest.CreateBadRequest("Invalid request", "Invalid request", 400);
         }
     }
 }
